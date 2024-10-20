069bef0967477917f42b929058c002dae6bfbdbb
Irina <sergei32git@gmail.com>
2024-10-20 22:26:44 +0500

    Пуш новых тестов (другие даты, города, двойная фамилия)

--- src/test/java/ru/netology/DeliveryTestPositive.java
+++ src/test/java/ru/netology/DeliveryTestPositive.java
@@ -31,9 +31,60 @@ public class DeliveryTestPositive {
         $(".notification__content").shouldHave(text("Встреча успешно забронирована на " + date),
                 Duration.ofSeconds(15)).shouldBe(visible);
     }
-
-
-
-
-
+    @Test
+    void positiveTestV2() {
+        String date = setLocalDate(4);
+        open("http://localhost:9999");
+        $("[data-test-id=city] input").setValue("Казань");
+        $("[data-test-id=date] input").doubleClick().sendKeys(date);
+        $("[data-test-id=name] input").setValue("Новиков Сергей");
+        $("[data-test-id=phone] input").setValue("+79993454554");
+        $("[data-test-id=agreement]").click();
+        $$("button").find(exactText("Забронировать")).click();
+        $(byText("Успешно!")).shouldBe(visible, Duration.ofMillis(15000));
+        $(".notification__content").shouldHave(text("Встреча успешно забронирована на " + date),
+                Duration.ofSeconds(15)).shouldBe(visible);
+    }
+    @Test
+    void positiveTestV3() {
+        String date = setLocalDate(3);
+        open("http://localhost:9999");
+        $("[data-test-id=city] input").setValue("Казань");
+        $("[data-test-id=date] input").doubleClick().sendKeys(date);
+        $("[data-test-id=name] input").setValue("Новиков-Новиков Сергей");
+        $("[data-test-id=phone] input").setValue("+79993454554");
+        $("[data-test-id=agreement]").click();
+        $$("button").find(exactText("Забронировать")).click();
+        $(byText("Успешно!")).shouldBe(visible, Duration.ofMillis(15000));
+        $(".notification__content").shouldHave(text("Встреча успешно забронирована на " + date),
+                Duration.ofSeconds(15)).shouldBe(visible);
+    }
+    @Test
+    void positiveTestV4() {
+        String date = setLocalDate(5);
+        open("http://localhost:9999");
+        $("[data-test-id=city] input").setValue("Москва");
+        $("[data-test-id=date] input").doubleClick().sendKeys(date);
+        $("[data-test-id=name] input").setValue("Новиков-Новиков Сергей");
+        $("[data-test-id=phone] input").setValue("+79993454554");
+        $("[data-test-id=agreement]").click();
+        $$("button").find(exactText("Забронировать")).click();
+        $(byText("Успешно!")).shouldBe(visible, Duration.ofMillis(15000));
+        $(".notification__content").shouldHave(text("Встреча успешно забронирована на " + date),
+                Duration.ofSeconds(15)).shouldBe(visible);
+    }
+    @Test
+    void positiveTestV5() {
+        String date = setLocalDate(10);
+        open("http://localhost:9999");
+        $("[data-test-id=city] input").setValue("Санкт-Петербург");
+        $("[data-test-id=date] input").doubleClick().sendKeys(date);
+        $("[data-test-id=name] input").setValue("Новиков Сергей");
+        $("[data-test-id=phone] input").setValue("+79993454554");
+        $("[data-test-id=agreement]").click();
+        $$("button").find(exactText("Забронировать")).click();
+        $(byText("Успешно!")).shouldBe(visible, Duration.ofMillis(15000));
+        $(".notification__content").shouldHave(text("Встреча успешно забронирована на " + date),
+                Duration.ofSeconds(15)).shouldBe(visible);
+    }
 }
